596aaab2c38e0cc4e07457a8d70afd7ecf907f5e
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-07-22 01:42:13 -0400

    finished stylings

--- MEDIUM/global_components/MovieDetails.jsx
+++ MEDIUM/global_components/MovieDetails.jsx
@@ -34,15 +34,18 @@ class MovieDetails extends Component {
       return <div>Loading...</div>;
     }
     return (
-      <html className="w-full h-screen bg-blue-950 text-white">
-        <div className="flex flex-col items-center ml-96 mr-96">
+      <html className="w-full h-screen bg-blue-950 text-white font-mono">
+        <div className="flex flex-row items-center pt-40 pl-96 pr-96">
           <img
             src={`https://image.tmdb.org/t/p/w440_and_h660_face${movie.poster_path}`}
             alt={movie.title}
+            className="rounded-3xl"
           />
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
-          <p>{movie.release_date}</p>
+          <div className="bg-black rounded-3xl text-center pt-14 pb-14">
+            <h1 className="text-3xl">{movie.title}</h1>
+            <p>{movie.release_date}</p>
+            <p>{movie.overview}</p>
+          </div>
         </div>
       </html>
     );
--- MEDIUM/global_components/MovieResults.jsx
+++ MEDIUM/global_components/MovieResults.jsx
@@ -6,10 +6,10 @@ class MovieResults extends Component {
     const { movies } = this.props;
     return (
       <div>
-        <h1 className="text-white text-center font-mono">
-          Here are your Results!
+        <h1 className="text-white font-mono text-center">
+          Type in the Search bar!
         </h1>
-        <ul className="flex flex-auto text-white text-center font-mono phonemin:grid grid-flow-row grid-cols-10 mx-0 relative">
+        <ul className="flex flex-auto text-white font-mono phonemin:grid grid-flow-row grid-cols-9 relative justify-center">
           {movies && movies.length > 0 ? (
             movies.map((movie) => (
               <li key={movie.id}>
@@ -17,13 +17,14 @@ class MovieResults extends Component {
                   <img
                     src={`https://image.tmdb.org/t/p/w440_and_h660_face${movie.poster_path}`}
                     alt={movie.title}
+                    className="rounded-3xl"
                   />
                   {movie.title}
                 </NavLink>
               </li>
             ))
           ) : (
-            <li>No results found</li>
+            <li className="col-start-5 col-end-6">No results found</li>
           )}
         </ul>
       </div>
